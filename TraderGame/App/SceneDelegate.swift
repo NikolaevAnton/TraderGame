0cfbd2355aadce8dd2e2ee4c97af3530ec726d75
NikolaevAnton <mjop41@gmail.com>
2022-03-07 18:21:12 +0300

    Start project

---
+++ TraderGame/App/SceneDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  SceneDelegate.swift
+//  TraderGame
+//
+//  Created by Anton Nikolaev on 07.03.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+    }
+
+
+}
+
